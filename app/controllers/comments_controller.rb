bbd0512c18ec3cd51736d19139b59b5ce561475e
Martine9670 <pinnamartine@free.fr>
2026-03-02 17:28:09 +0100

    Add destroy action to comments routes to fix 404 error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!, only: [:create]
-
+    before_action :authenticate_user!, only: [:create, :destroy]
+    
   # POST /articles/:article_id/comments
   def create
     @article = Article.find(params[:article_id])
@@ -13,6 +13,19 @@ class CommentsController < ApplicationController
       render json: @comment.errors, status: :unprocessable_entity
     end
   end
+
+  # DELETE /articles/:article_id/comments/:id
+    def destroy
+    @comment = Comment.find(params[:id])
+
+    # Vérification : Seul l'auteur du commentaire peut le supprimer
+    if @comment.user == current_user
+        @comment.destroy
+        render json: { message: "Commentaire supprimé avec succès" }, status: :ok
+    else
+        render json: { error: "Tu ne peux pas supprimer le commentaire d'un autre !" }, status: :forbidden
+    end
+end
 
   private
 
